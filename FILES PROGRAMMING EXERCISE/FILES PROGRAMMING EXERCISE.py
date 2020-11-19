819a03b2bbebe51c386419724d89750cdf83ec1e
hassan701 <71362199+hassan701@users.noreply.github.com>
2020-11-20 01:21:23 +0300

    new

---
+++ FILES PROGRAMMING EXERCISE/FILES PROGRAMMING EXERCISE.py
@@ -0,0 +1,34 @@
+def word_count(text):
+    mydict = {}
+    hapax = []
+    key = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v','w','x', 'y' , 'z']
+    for x in range(len(text)):
+        word = ''
+        for i in text[x]:
+            if i in key:
+                word = word + i
+        mydict[word] = mydict.get(word, 0) + 1
+    for wo,nu in mydict.items():
+        if nu == 1:
+            hapax.append(wo)
+    return hapax
+def main():
+    file = open('pg63807.txt', encoding='utf8')
+    text = file.read().lower().replace('\n', ' ').split(' ')
+    #print(text)
+    print(word_count(text))
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+#punctuation ={"'","!",'"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'}
